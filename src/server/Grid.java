3001103afe6b2e9aeccd5db79b23e7e8242d4559
aidankirk617 <aidankirk617@gmail.com>
2021-11-16 14:41:23 -0500

    Grid is finished

--- src/server/Grid.java
+++ src/server/Grid.java
@@ -1,38 +1,75 @@
 /**
- * BattleClient
+ * Grid
  * @author Aidan Kirk, David Jennings
- * @version 11/12/21
+ * @version 11/16/21
  */
 
 package server;
 
+import java.util.Random;
+import java.util.Scanner;
+
+/** TODO
+ * Note to David: Either put ship data in this file, or make separate standalone file
+ */
+
 public class Grid {
 
-  private int level;
-  private int boardSize;
-  private int[][] gameBoard;
-  private int shipSize;
-  private int shipMarker;
-  private String shipString;
+  private char[][] board;   // Game board used to play battleship
+  private int size;         // Size of the game board
 
-  public static final int NUM_ROWS = 10;
-  public static final int NUM_COLS = 10;
+  // Constructor
+  public Grid(int size) {
+    // Initialize Gameboard
+    board = new char[size][size]; // Take size of array
+    this.size = size;
 
-  public void fillBoard() {
-    for (int i = 0; i < boardSize; i++) {
-      for (int j = 0; j < boardSize; j++) {
-        gameBoard[i][j] = 0;
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        board[i][j] = ' ';   // Initialize length of array
       }
     }
   }
 
-  public void printToScreen() {
+  public Grid(int size, char init) {
 
-    System.out.print("   ");
-    for (int i = 0; i < gameBoard[0].length; i++) {
-      System.out.println("+---+---+---+---+---+---+---+---+---+---+");
-      System.out.println("|");
-      System.out.printf("%3d", i + 1);
+    board = new char[size][size];   // Possibly go back and just make this size 10?
+    this.size = size;
+
+    for (int i = 0; i < board.length; i++) {
+      for (int j = 0; j < board[i].length; j++) {
+        board[i][j] = init;   // Populate board with char init
+      }
     }
+  }
+
+  public void print() {
+    //holds
+    System.out.print("    ");
+    String graphic = "  ";
+
+    for (int i = 0; i < board.length; i++) {
+      System.out.print(i + "   ");
+      graphic += "+---";    // Graphic for the game board
+    }
+
+    graphic += "+";         // Graphic for the game board
+    System.out.println();
+
+    for (int i = 0; i < board.length; i++) {
+      System.out.println(graphic);
+      System.out.print(i + " ");
+      for (int j = 0; j < board[i].length; j++) {
+        System.out.print("| " + board[i][j] + " ");
+      }
+      System.out.print("|");
+      System.out.println();
+    }
+    System.out.println(graphic);
+  }
+
+  public static void main(String[] args) {
+    Grid grid = new Grid(10);    // Output 9x9 game board
+    grid.print();                     // Prints game board
   }
 }

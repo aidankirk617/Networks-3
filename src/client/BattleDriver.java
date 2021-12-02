1f46bba12c04d2702566a8192183fe29da7d1fd7
David Jennings <davidjjennings99@gmail.com>
2021-12-01 19:46:51 -0500

    BattleDriver now accepts cla

--- src/client/BattleDriver.java
+++ src/client/BattleDriver.java
@@ -8,9 +8,41 @@
 
 package client;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class BattleDriver {
 
     public static void main(String[] args) {
+
+        if (args.length != 3){
+            System.out.println("Please enter the correct number of arguments");
+            System.exit(1);
+        }
+
+        InetAddress host = null;
+        try {
+            host = InetAddress.getByName(args[1]);
+        }catch (UnknownHostException unknownHostException){
+            System.out.println("Unknown Host");
+            System.exit(1);
+        }
+
+        int port = 0;
+        if(args[1].length() != 4){
+            System.out.println("Invalid port: ports must be 4 numbers");
+        }
+        try{
+            port = Integer.parseInt(args[1]);
+        }catch (NumberFormatException numberFormatException){
+            System.out.println("Port number can only contain integers");
+            System.exit(1);
+        }
+
+        String username = args[2];
+
+        System.out.println("Hello " + username + ", connecting you to: " + host.getHostName() +
+                " on port: " + port);
 
     }
 }
